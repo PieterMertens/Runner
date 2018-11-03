74453eb978efcb91ad697fa79bdc78b2406029d1
Pieter <pieter_mertens13@hotmail.com>
2018-11-03 20:29:59 +0100

    mannetje
    loopbeweging

---
+++ Assets/Scripts/cameraMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour {
+
+    private Transform lookAt;
+    private Vector3 startOffset;
+    private Vector3 moveVector;
+
+    private float transition = 0.0f;
+    private float animationDuration = 2.0f;
+    private Quaternion targetRotation = Quaternion.Euler(0,0,0);
+    private Vector3 cameraOffset = new Vector3(0,3,-5);
+
+    private float targetDistance = 5f;
+
+	// Use this for initialization
+	void Start () {
+        lookAt = GameObject.FindGameObjectWithTag("Player").transform;
+        startOffset = transform.position - lookAt.position;
+
+        //print("--- targetrot" + targetRotation.ToString());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveVector = lookAt.position + cameraOffset;
+
+        //X
+        moveVector.x = 0;
+
+        //Y
+        moveVector.y = Mathf.Clamp(moveVector.y,-4,10);
+
+        /*
+        if (transition > 1f) {
+            transform.position = moveVector;
+        } else {
+            moveVector = lookAt.position;
+            transform.RotateAround(lookAt.position, lookAt.up, 180 / animationDuration * Time.deltaTime  );
+            //Vector3 delta = transform.position - lookAt.position;
+            //transform.position = transform.position + delta.normalized * targetDistance;
+
+
+            transform.position = Vector3.Lerp(transform.position , transform.position + cameraOffset, transition);
+            transition += Time.deltaTime / animationDuration;
+           
+
+        }
+        */
+
+        transform.position = moveVector;
+        transform.rotation = Quaternion.Euler(0,0,0);
+    		
+	}
+}
---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+
+    private CharacterController controller;
+    private Vector3 moveVector;
+
+    public float speed = 3f;
+
+    private float gravity = -10f;
+    private float verticalVelocity = 0f;
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveVector = Vector3.zero;
+
+        //X
+        moveVector.x = Input.GetAxisRaw("Horizontal");
+
+        //Y
+        if (!controller.isGrounded) {
+            verticalVelocity += gravity * Time.deltaTime;
+        }
+        moveVector.y = verticalVelocity;
+
+        //Z
+        moveVector.z = speed;
+
+        controller.Move(moveVector * Time.deltaTime);
+	}
+}
