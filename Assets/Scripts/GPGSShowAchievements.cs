677331522f6474e17baac2b984d3263bf4ce6ff4
Pieter <pieter_mertens13@hotmail.com>
2018-12-05 12:50:25 +0100

    gpgps en menus

---
+++ Assets/Scripts/GPGSShowAchievements.cs
@@ -0,0 +1,79 @@
+﻿using GooglePlayGames;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GPGSShowAchievements : MonoBehaviour {
+
+    public Button achievementsButton;
+    public Text achievementsButtonText;
+
+    public Button leaderboardButton;
+    public Text leaderboardButtonsText;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void ShowLeaderboards()
+    {
+        if (PlayGamesPlatform.Instance.localUser.authenticated) {
+            PlayGamesPlatform.Instance.ShowLeaderboardUI();
+        } else {
+
+            leaderboardButtonsText.text = "Sing in first";
+
+
+            StartCoroutine(leaderboardText(1f));
+
+                        Debug.Log("Cannot show leaderboard: not authenticated");
+        }
+    }
+
+    IEnumerator leaderboardText(float seconds)
+    {
+
+        yield return new WaitForSeconds(seconds);
+        leaderboardButtonsText.text = "Highscores";
+
+
+    }
+
+    public void ShowAchievements()
+    {
+        if (PlayGamesPlatform.Instance.localUser.authenticated) {
+            PlayGamesPlatform.Instance.ShowAchievementsUI();
+        } else {
+            achievementsButtonText.text = "Sing in first";
+
+
+           StartCoroutine(achievementsText(1f));
+                    
+
+            Debug.Log("Cannot show Achievements, not logged in");
+        }
+    }
+
+    IEnumerator achievementsText(float seconds)
+    {
+      
+        yield return new WaitForSeconds(seconds);
+        achievementsButtonText.text = "Achievements";
+
+
+    }
+
+
+
+
+
+
+}
---
+++ Assets/Scripts/GPGSSignOut.cs
@@ -0,0 +1,64 @@
+﻿using GooglePlayGames;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GPGSSignOut : MonoBehaviour {
+
+    public Text signInButtonText;
+
+
+    // Use this for initialization
+    void Start() {
+
+        if (PlayGamesPlatform.Instance.localUser.authenticated) {
+            signInButtonText.text = "Sign out from Google Play Services";
+        } else {
+        
+            signInButtonText.text = "Sign in on Google Play Services";
+            
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void SignInCallback(bool success)
+    {
+        if (success) {
+            Debug.Log("################## INFIRUNNER Signed in!");
+
+            // Change sign-in button text
+            signInButtonText.text = Social.localUser.userName+ " Sign out";
+
+            // Show the user's name
+            //authStatus.text = "Signed in as: " + Social.localUser.userName;
+        } else {
+            Debug.Log("################## INFIRUNNER Sign-in failed...");
+
+            // Show failure message
+            signInButtonText.text = "Sign in on Google Play Services";
+            //authStatus.text = "Sign-in failed";
+        }
+    }
+
+    public void SignIn()
+    {
+        if (!PlayGamesPlatform.Instance.localUser.authenticated) {
+            // Sign in with Play Game Services, showing the consent dialog
+            // by setting the second parameter to isSilent=false.
+            PlayGamesPlatform.Instance.Authenticate(SignInCallback, false);
+        } else {
+            // Sign out of play games
+            PlayGamesPlatform.Instance.SignOut();
+
+            // Reset UI
+            signInButtonText.text = "Sign in on Google Play Services";
+            //authStatus.text = "";
+        }
+    }
+}
